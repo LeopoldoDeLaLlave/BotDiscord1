70e830a12ea91adfbedfd8ea52e84703cf26f363
LeopoldoDeLaLlave <58303142+LeopoldoDeLaLlave@users.noreply.github.com>
2020-08-31 17:43:16 +0200

    Si envías !joke te cuenta un chiste el bot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const fetch = require('node-fetch');
 var testCha = null;
 
 client.on('ready', () => {
@@ -20,27 +21,38 @@ client.on('ready', () => {
 });
 
 //Hay que poner el token que te da discord
-client.login('Token');
+client.login('token');
 
 client.on('message', async message => {
     //console.log(message.content);
+
+    //Si dices ping responde pong!
     if (message.content === 'ping') {
         message.reply('Pong!');
     }
+
+    //Devuelve el saludo
     if (message.content === 'Hola') {
         message.channel.send(`Hola, ${message.author}`);
     }
 
 
-
+    //Da info inútil
     if (message.content === '!chickles') {
         message.channel.send('https://info.mercadona.es/es/consejos/alimentacion/chicles-con-100-xilitol-complementa-tu-cuidado-dental/tip');
     }
 
+    //Cuenta un chiste
+    if (message.content === '!joke') {
+        chiste(message);
+    }
+
+    //Simplemente para testear
     if (message.content.startsWith('!test')) {
         message.channel.send('Respuesta test');
     }
 
+    //Para probar la edición de mensajes
     if (message.content === '!pretty') {
         const embed = new Discord.MessageEmbed()
             // Set the title of the field
@@ -54,13 +66,14 @@ client.on('message', async message => {
         message.channel.send(embed);
     }
 
+    //Borra los mensajes del chat
     if (message.content === '!clear') {
 
         try {
             const fetched = await message.channel.messages.fetch();
             message.channel.bulkDelete(fetched);
             message.channel.send("Borrados todos los mensajes");
-        } catch(err){
+        } catch (err) {
             message.channel.send("No se pudieron borrar los mensajes");
             console.log(err);
         }
@@ -68,3 +81,9 @@ client.on('message', async message => {
     }
 
 });
+
+//Coge un chiste random de la API y lo envía
+async function chiste(msg) {
+    const { joke } = await fetch('https://sv443.net/jokeapi/v2/joke/Any?type=single').then(response => response.json());
+    msg.channel.send(joke);
+}
